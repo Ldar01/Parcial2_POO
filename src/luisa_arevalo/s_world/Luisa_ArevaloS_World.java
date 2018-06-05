0996f4b833a8a7b811e29749159ce81389607d08
Luisa <Luisa@192.168.56.1>
2018-06-04 22:36:15 -0600

    No se hizo mucho en el main ya que no ha sido probado

---
+++ src/luisa_arevalo/s_world/Luisa_ArevaloS_World.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package luisa_arevalo.s_world;
+
+/**
+ *
+ * @author Luisa Arevalo <00038617@uca.edu.sv>
+ */
+public class Luisa_ArevaloS_World {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
