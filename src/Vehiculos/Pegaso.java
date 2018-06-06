7463e75013b7c18ec959f7c9b2828e1634cd6058
Luisa <Luisa@192.168.56.1>
2018-06-05 18:39:14 -0600

    Se creo la clase Pegaso

---
+++ src/Vehiculos/Pegaso.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Vehiculos;
+
+/**
+ *
+ * @author Luisa Arevalo <00038617@uca.edu.sv>
+ */
+public class Pegaso implements Vehiculo{
+
+    @Override
+    public void Transportar() {
+        System.out.println("*Flies in the air*");
+    }
+    
+}
