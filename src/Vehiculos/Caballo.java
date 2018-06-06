2a46c661dc2d99f2af6d590be852bb60125ec653
Luisa <Luisa@192.168.56.1>
2018-06-05 18:38:58 -0600

    Se creo la clase Caballo

---
+++ src/Vehiculos/Caballo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Vehiculos;
+
+/**
+ *
+ * @author Luisa Arevalo <00038617@uca.edu.sv>
+ */
+public class Caballo implements Vehiculo{
+
+    @Override
+    public void Transportar() {
+        System.out.println("*Track track track*");}
+    
+}
