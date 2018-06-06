cd3705d9b6982335bc3ca69137f63c66136ef82d
Luisa <Luisa@192.168.56.1>
2018-06-05 18:40:32 -0600

    Se realizaron unas cuantas modificaciones

--- src/luisa_arevalo/s_world/FabricaAbstracta.java
+++ src/luisa_arevalo/s_world/FabricaAbstracta.java
@@ -6,10 +6,22 @@
 
 package luisa_arevalo.s_world;
 
+import Edificaciones.Edificaciones_Accion;
+import Recursos.Recursos_de_razas;
+import Reinos.Reino_escogido;
+import Vehiculos.Vehiculo;
+import Unidades.Unidad_accion;
+import Unidades.Unidades;
+
 /**
  *
  * @author Luisa Arevalo <00038617@uca.edu.sv>
  */
 public interface FabricaAbstracta {
-
+    Reino_escogido getReino(String nombre_reino);
+    Edificaciones_Accion getEdificacion(String nombre_edificacion);
+    Recursos_de_razas getRecurso(String nombre_recurso);
+    Unidades getUnidad(String nombre_unidad);
+    Vehiculo getVehiculo(String nombre_vehiculo);
+    
 }
