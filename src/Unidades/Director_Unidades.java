eecc1237f5d8fc67523d7c491baf94ca72ffa93e
Luisa <Luisa@192.168.56.1>
2018-06-07 21:50:15 -0600

    Se creo la clase Director_Unidades

---
+++ src/Unidades/Director_Unidades.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Unidades;
+
+/**
+ *
+ * @author Luisa Arevalo <00038617@uca.edu.sv>
+ */
+public class Director_Unidades {
+
+    public Director_Unidades() {}
+    
+    public void Construir( BuilderUnidades build){
+        build.CrearNuevaUnidad();
+        build.nombre_Unidad();
+        build.Puntos_Vitales();
+        build.atacar();
+        build.defender();
+    }
+    
+}
