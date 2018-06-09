50a9a4856a6292a8b88f481ebe9d613694b9220a
Luisa <Luisa@192.168.56.1>
2018-06-08 22:35:20 -0600

    Se quito la FabricaAbstracta y FabricaProductora temporalmente, es probable que se ocupe luego

--- src/luisa_arevalo/s_world/Luisa_ArevaloS_World.java
+++ src/luisa_arevalo/s_world/Luisa_ArevaloS_World.java
@@ -3,13 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package luisa_arevalo.s_world;
-
-import Unidades.BuilderUnidades;
-import Unidades.Director_Unidades;
-import Unidades.Mercenario;
-import Unidades.Unidades;
 
 /**
  *
@@ -21,20 +15,6 @@ public class Luisa_ArevaloS_World {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Director_Unidades objFabrica = new Director_Unidades();
-        BuilderUnidades mercenario = new Mercenario();
-        
-        objFabrica.Construir(mercenario);
-        Unidades mer = mercenario.getUnidad();
-        
-        mostrarCaracteristicas(mer);
-    }
     
-    public static void mostrarCaracteristicas(Unidades unidad){
-        System.out.println("Nombre:\t"+ unidad.getNombre_Unidad());
-        System.out.println("Puntos Vitales:\t"+ unidad.getPV());
-        System.out.println("Ataque:\t"+ unidad.getAtk());
-        System.out.println("Defensa:\t"+ unidad.getDef());
     }
-
 }
