433827a515dbecdf84c8a4ba4b56eb5b45715af5
Luisa <Luisa@192.168.56.1>
2018-06-11 22:52:58 -0600

    Se hicieron algunas opciones

--- src/luisa_arevalo/s_world/Menu.java
+++ src/luisa_arevalo/s_world/Menu.java
@@ -6,6 +6,8 @@
 
 package luisa_arevalo.s_world;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Luisa Arevalo <00038617@uca.edu.sv>
@@ -23,6 +25,34 @@ public class Menu {
             System.out.println("No se puede crear otro objeto Menu porque ya existe");
         }
         return menu;
+    }
+    
+    public void Inicio(){
+        int opcion;
+        Scanner read = new Scanner(System.in);
+        System.out.println("\t============ Bienvenido a Luisa's World ===============\n\n"
+                + "\t 1) Jugar\n"
+                + "\t 2) Salir\n\n");
+        System.out.print("\tOpcion: ");
+        try{
+            opcion = read.nextInt();
+            switch(opcion){
+                case 1:
+                    break;
+                case 2:
+                    System.out.println("Saliendo del juego.......");
+                    break;
+                default:
+                    System.err.println("Opcion no valida");
+            }
+        }catch (Exception e) {
+            System.err.println("Lo que ingresaste no es un numero");
+            read.nextLine();
+        }
+    }
+    
+    public void Opciones_Jugador(){
+        System.out.println("=========");
     }
     
     
