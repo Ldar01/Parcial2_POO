1fe16be157d31953a167d4b70c43562ed26ed6b3
Luisa <Luisa@192.168.56.1>
2018-06-11 22:52:39 -0600

    Se probo la manera de golpear a un enemigo

--- src/luisa_arevalo/s_world/Luisa_ArevaloS_World.java
+++ src/luisa_arevalo/s_world/Luisa_ArevaloS_World.java
@@ -5,6 +5,12 @@
  */
 package luisa_arevalo.s_world;
 
+import Edificaciones.Edificacion;
+import Unidades.BuilderUnidades;
+import Unidades.Director_Unidades;
+import Unidades.Mercenario;
+import Unidades.Unidades;
+
 /**
  *
  * @author Luisa Arevalo <00038617@uca.edu.sv>
@@ -15,6 +21,37 @@ public class Luisa_ArevaloS_World {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+//        Menu m = Menu.getInstance();
+//        m.Inicio();
         
+        
+        Director_Unidades fabrica = new Director_Unidades();
+        BuilderUnidades mercenario = new Mercenario();
+        fabrica.Construir(mercenario);
+        Unidades p = mercenario.getUnidad();
+        Unidades pedro = new Unidades();
+        Edificacion pa = new Edificacion();
+        System.out.println(pedro.getPV());
+//        String clase, clase2;
+//        clase = pa.getClass().getName();
+//        clase2 = p.getClass().getName();
+//        System.out.println(clase);
+//        System.out.println(clase2);
+//        if(clase == clase2){
+//            System.out.println("Misma clase");
+//        }
+//        else{
+//            System.out.println("Nop");
+//        }
+        p.atacar(pedro);
+        System.out.println("La vida de la unidad ahora es: "+pedro.getPV());
+        p.atacar(pedro);
+        System.out.println("La vida de la unidad ahora es: "+pedro.getPV());
+        p.atacar(pedro);
+        System.out.println("La vida de la unidad ahora es: "+pedro.getPV());
+        p.atacar(pedro);
+        System.out.println("La vida de la unidad ahora es: "+pedro.getPV());
+        p.atacar(pedro);
+        System.out.println("La vida de la unidad ahora es: "+pedro.getPV());
     }
 }
